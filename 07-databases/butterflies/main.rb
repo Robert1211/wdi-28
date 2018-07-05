0e34f6850aeb5ea8359f90abf44f61ab99de5e41
JT <certainthings@gmail.com>
2018-07-05 14:48:30 +1000

    butterflies show and create

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -16,7 +16,23 @@ get '/butterflies' do
   erb :butterflies_index
 end
 
+# New (needs to come before /butterflies/:id)
+get '/butterflies/new' do
+  erb :butterflies_new
+end
+
+# Create
+post '/butterflies' do
+  db = SQLite3::Database.new 'database.sqlite3'
+  db.execute "INSERT INTO butterflies (name, family, image) VALUES ('#{ params[:name] }', '#{ params[:family] }', '#{ params[:image] }')"
+  redirect to('/butterflies') # This will be a GET request
+end
+
 # Show
 get '/butterflies/:id' do
-  params[:id].to_s
+  db = SQLite3::Database.new 'database.sqlite3'
+  db.results_as_hash = true
+  @butterfly = db.execute "SELECT * FROM butterflies WHERE id=#{ params[:id] }"
+  @butterfly = @butterfly.first # Strip off the outer array
+  erb :butterflies_show
 end
