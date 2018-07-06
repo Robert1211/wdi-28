cbc45e98ec565e39c6a412aa966a056c902e5020
JT <certainthings@gmail.com>
2018-07-06 11:34:15 +1000

    inheritance example

--- 05-ruby/classes.rb
+++ 05-ruby/classes.rb
@@ -1,6 +1,16 @@
 require 'pry'
 
-class MarxBrother
+class Actor
+  def award_speech
+    "I would like to thank my parents and my agent. We did it baby"
+  end
+
+  def deny_allegations
+    "I deny that and I was drunk and I don't remember that anyway"
+  end
+end
+
+class MarxBrother < Actor
   # macro writes @name, @instrument and @vice getters and setters for you
   attr_accessor :name, :instrument, :vice
 
@@ -13,7 +23,12 @@ class MarxBrother
   def play
     "My name is #{ @name } and I play the #{ @instrument }"
   end
+end
 
+class Stooge < Actor
+  def terrible?
+    true
+  end
 end
 
 chico = MarxBrother.new 'Chico Marx', 'piano', 'gambling'
