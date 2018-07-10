59e34f1343df38e69a8602168c678e5df5e89326
JT <certainthings@gmail.com>
2018-07-10 11:01:00 +1000

    rails view helpers

---
+++ 08-rails/view_helpers/app/controllers/pages_controller.rb
@@ -0,0 +1,27 @@
+class PagesController < ApplicationController
+  def home
+    render :home
+  end
+
+  def numbers
+    @amount = 234.034348
+    @large_number = 4240342329528
+    @phone = 2125551212
+    render :numbers
+  end
+
+  def text
+    @numbers = (1..20).to_a.shuffle
+    @friend_count = 1
+    @enemies_count = 15
+    @story = "A very long long long long long long long long long long long long time ago"
+  end
+
+  def assets
+
+  end
+
+  def url
+  end
+
+end
