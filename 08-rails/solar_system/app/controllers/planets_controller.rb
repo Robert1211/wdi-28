62f7543700b5ac1bf42fd3b88f5994eedb2d4041
JT <certainthings@gmail.com>
2018-07-10 14:54:56 +1000

    planets CRUD

--- 08-rails/solar_system/app/controllers/planets_controller.rb
+++ 08-rails/solar_system/app/controllers/planets_controller.rb
@@ -18,7 +18,29 @@ class PlanetsController < ApplicationController
     planet.diameter = params[:diameter]
     planet.mass = params[:mass]
     planet.moons = params[:moons]
-    planet.save
+    planet.save # INSERT
     redirect_to planet_path(planet.id)
+  end
+
+  def edit
+    @planet = Planet.find params[:id]
+  end
+
+  def update
+    planet = Planet.find params[:id]
+    planet.name = params[:name]
+    planet.image = params[:image]
+    planet.orbit = params[:orbit]
+    planet.diameter = params[:diameter]
+    planet.mass = params[:mass]
+    planet.moons = params[:moons]
+    planet.save # UPDATE
+    redirect_to planet_path(planet.id)
+  end
+
+  def destroy
+    planet = Planet.find params[:id]
+    planet.destroy
+    redirect_to planets_path
   end
 end
