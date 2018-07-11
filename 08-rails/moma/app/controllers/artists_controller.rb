f774227b5fea95d2a5ffa1c7b6032dff58c4df93
JT <certainthings@gmail.com>
2018-07-11 12:35:52 +1000

    artist and works, index and show

---
+++ 08-rails/moma/app/controllers/artists_controller.rb
@@ -0,0 +1,15 @@
+class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find params[:id]
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
