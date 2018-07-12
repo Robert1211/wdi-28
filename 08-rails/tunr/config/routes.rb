ea618f9ee20ea5a166fe67a16dfe3dc7bb91633b
JT <certainthings@gmail.com>
2018-07-12 15:17:55 +1000

    user login and session controller

--- 08-rails/tunr/config/routes.rb
+++ 08-rails/tunr/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'session/new'
   root :to => 'pages#home'
   resources :users, :only => [:new, :create]
+
+  get '/login' => 'session#new' # login form
+  post '/login' => 'session#create' # perform a login
+  delete '/login' => 'session#destroy' # perform a log out
 end
