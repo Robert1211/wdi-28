93e77df390ca0d869d385582cfb6e5879a79647d
JT <certainthings@gmail.com>
2018-07-13 11:59:03 +1000

    admins and redirects

--- 08-rails/tunr/app/controllers/mixtapes_controller.rb
+++ 08-rails/tunr/app/controllers/mixtapes_controller.rb
@@ -1,12 +1,14 @@
 class MixtapesController < ApplicationController
+  before_action :check_for_login, :only => [:new, :create]
+
   def new
     @mixtape = Mixtape.new
   end
 
   def create
-    mixtape = Mixtape.create mixtape_params
-    @current_user.mixtapes << mixtape
-    # mixtape = @current_user.mixtapes.create mixtape_params
+    # mixtape = Mixtape.create mixtape_params
+    # @current_user.mixtapes << mixtape
+    mixtape = @current_user.mixtapes.create mixtape_params
     redirect_to root_path
   end
 
