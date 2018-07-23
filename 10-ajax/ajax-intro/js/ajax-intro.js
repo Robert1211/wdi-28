62af638cd9027a03ffa2d95f84094cc5979b3f45
JT <certainthings@gmail.com>
2018-07-23 14:42:50 +1000

    vanilla AJAX example with numbers API

--- 10-ajax/ajax-intro/js/ajax-intro.js
+++ 10-ajax/ajax-intro/js/ajax-intro.js
@@ -1,13 +1,24 @@
-const xhr = new XMLHttpRequest();
+const fetchFact = function () {
+  const xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState !== 4) {
-    return;
-  }
+  // This callback function is run every time the readyState changes (i.e. from 0..4);
+  xhr.onreadystatechange = function () {
+    // Ignore all readyStates except 4.
+    if (xhr.readyState !== 4) {
+      return;
+    }
 
-  // we can safely access xhr.responseText here
-  document.getElementById('result').innerHTML = xhr.responseText;
+    const info = JSON.parse( xhr.responseText );
+
+    // When we get here, the responseText should have a value from the remote server.
+    const p = document.createElement('p');
+    p.innerHTML = '<strong>' + info.number + '</strong>: ' + info.text;
+    document.body.appendChild( p );
+  };
+
+  xhr.open('GET', 'http://numbersapi.com/random/trivia?json');
+  xhr.send(); // Asynchronous: runs in the background
 };
 
-xhr.open('GET', 'http://numbersapi.com/random/trivia');
-xhr.send(); // Asynchronous: runs in the background
+document.getElementById('fetch').addEventListener('click', fetchFact);
+fetchFact();
