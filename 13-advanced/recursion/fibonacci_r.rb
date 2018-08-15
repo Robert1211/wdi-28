dbcf638003e423491b15dac57c21446af3064b7f
JT <certainthings@gmail.com>
2018-08-16 09:35:30 +1000

    slow fibonacci

--- 13-advanced/recursion/fibonacci_r.rb
+++ 13-advanced/recursion/fibonacci_r.rb
@@ -1,8 +1,20 @@
+def fibonacci_iterative(n)
+  a = 1
+  b = 1
+  while n > 2 # Base case
+    a, b = b, a+b # Parallel assignment
+    n -= 1 # Move toward the base case
+  end
+
+  b
+end
+
 def fibonacci_recursive(n)
-  # Base case
-  # Move toward the base case
-  # Recurse
-  # Return the nth Fibonacci number
+  if n == 1 || n == 2 # Base case
+    1
+  else
+    fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2) # Recursive steps
+  end
 end
 
 require 'pry'
