e8e3a62e910dc4fd3adeb53462861f66aea14d7b
JT <certainthings@gmail.com>
2018-08-17 11:51:22 +1000

    google books search in express

--- 14-node/express-books/index.js
+++ 14-node/express-books/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -6,6 +7,20 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('pages/home');
+});
+
+app.get('/search', (req, res) => {
+  const title = req.query.title;
+  const booksURL = `https://www.googleapis.com/books/v1/volumes?q=${ title }`;
+
+  // Async
+  axios.get(booksURL).then((result) => {
+    const cover = result.data.items[0].volumeInfo.imageLinks.thumbnail;
+    res.render('pages/search', {
+      cover: cover, // a single value
+      bookInfo: result.data.items[0].volumeInfo // all of the first book info
+    });
+  });
 });
 
 console.log('http://localhost:8888/');
