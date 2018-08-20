314830aa5b5ad208fd7d8f194915c19a531bce58
JT <certainthings@gmail.com>
2018-08-20 12:30:07 +1000

    bubble sort skellingtons

---
+++ 13-advanced/sorting/bubbleSort.js
@@ -0,0 +1,8 @@
+const a = [12, 242, 53, -12, 24, 33, 1001, 25, 78];
+
+const bubbleSort = (haystack) => {
+  // Returns a new array with haystack in sorted order (ascending)
+  // Use bubble sort (not .sort())
+};
+
+console.log( bubbleSort(a) );
---
+++ 13-advanced/sorting/bubble_sort.rb
@@ -0,0 +1,8 @@
+a = Array.new(100) { rand(1..1000) }
+
+def bubble_sort(haystack)
+  # Returns a new array with a in sorted order (ascending)
+  # Use bubble sort (not .sort)
+end
+
+puts bubble_sort( a ) == a.sort
