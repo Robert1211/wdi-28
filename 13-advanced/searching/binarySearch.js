0043b5ae2f558b48d2773fe9bfc9d06598b89b3d
JT <certainthings@gmail.com>
2018-08-20 11:53:41 +1000

    binary search skellingtons

---
+++ 13-advanced/searching/binarySearch.js
@@ -0,0 +1,8 @@
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 20, 24, 30];
+
+const binarySearch = (haystack, needle) => {
+  // Return the index of needle within the haystack
+  // or null if needle is not within the haystack
+}
+
+console.log( binarySearch(7) ); // => 6
---
+++ 13-advanced/searching/binary_search.rb
@@ -0,0 +1,10 @@
+a = Array.new(100) { rand(1..1000) }.sort.uniq
+
+def binary_search(haystack, needle)
+  # Return the index of needle within the haystack
+  # or nil if needle is not within haystack
+
+  # Your code goes here
+end
+
+puts binary_search(a, 15)
