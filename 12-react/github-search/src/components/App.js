5e04754a4024be0acd34f9bbae41cc097c08c73e
JT <certainthings@gmail.com>
2018-08-21 11:50:33 +1000

    example Hello component

--- 12-react/github-search/src/components/App.js
+++ 12-react/github-search/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Hello from './Hello';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>Goodbye Cruel World</h1>
+        <Hello />
       </div>
     );
   }
