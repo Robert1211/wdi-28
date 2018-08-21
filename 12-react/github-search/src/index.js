6793dcd5566874032b9723eb81eacd41a4faacf3
JT <certainthings@gmail.com>
2018-08-21 12:17:50 +1000

    search and home components complete

--- 12-react/github-search/src/index.js
+++ 12-react/github-search/src/index.js
@@ -5,12 +5,14 @@ import 'preact/devtools';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Home from './components/Home';
-// TODO: import Search and Profile
+import Search from './components/Search';
+// TODO: import Profile
 
 const Routes = (
   <Router>
     <div>
       <Route exact path="/" component={Home} />
+      <Route exact path="/search" component={Search} />
     </div>
   </Router>
 );
